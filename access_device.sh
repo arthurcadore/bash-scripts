4cb58c0687e08a12d43fec3d68d1e26901fe1356
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2023-07-12 17:30:08 -0300

    Update access_device.sh

--- access_device.sh
+++ access_device.sh
@@ -5,11 +5,14 @@
 # Execution example: ./access_device.sh 10.0.0.1 Admin@01
 
 # first of all, the script verifies if all arguments was passed: 
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
 	    echo "Its necessary to input IP address and password to execute!"
-	        echo "Command example: $0 <IP_address> <Password>"
+	        echo "Command example: $0 <IP_address> <Password> <Command-String>"
 		    exit 1
 fi
+
+# VariÃ¡veis de conexÃ£o SSH
+USUARIO="admin"
 
 # Get IP (First Argument) and put it to variable "IP":
 IP="$1"
@@ -17,15 +20,11 @@ IP="$1"
 # Get Password (Second Argument) and put it to variable "PASSW":
 PASSW="$2"
 
-# VariÃ¡veis de conexÃ£o SSH
-USUARIO="admin"
+# Get command string (Tird Argument) and put it to SSH sesion:
+COMMAND="$3"
+
 
 # Execute command, 
 sshpass -p "$PASSW" ssh -o StrictHostKeyChecking=no $USUARIO@$IP << EOF
-
-    logread
-    ubus call uci get {config:network}
-
+   $COMMAND
 EOF
-
-# echo "$OUTPUT"
