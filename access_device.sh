6dfb2ce78131103a0e6368591584908b700dc3bd
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2023-07-12 16:34:13 -0300

    Create access_device.sh

---
+++ access_device.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author:Arthur Cadore M. Barcella
+# Github: arthurcadore
+
+# Code: Acess device from ssh and aplly any command;
+
+# first of all, the script verifies if all arguments was passed: 
+if [ $# -ne 2 ]; then
+    echo "É necessário fornecer o IP e o comando como argumentos."
+    echo "Exemplo de uso: $0 endereço_IP 'system version'"
+    exit 1
+fi
+
+# Get IP (First Argument) and put it to variable "IP":
+IP="$1"
+
+# Get the command syntax (Second Argument) and put it to variable "IP":
+COMMAND="$2"
+
+# Variáveis de conexão SSH
+USUARIO="nome_de_usuário"
+SENHA="senha"
+
+# Execute command, 
+OUTPUT=$(sshpass -p "$SENHA" ssh -o StrictHostKeyChecking=no $USUARIO@$IP "$COMMAND")
+
+echo "$OUTPUT"
