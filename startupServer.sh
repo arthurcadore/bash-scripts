6368d7f2b9ab67362178c8b708a3b6e4bc593027
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2023-10-07 09:28:55 -0300

    Update startupServer.sh

--- startupServer.sh
+++ startupServer.sh
@@ -8,3 +8,14 @@ apt-get install -y fping tree arping net-tools vlan zabbix-agent htop unzip qemu
 
 # Enable capture on hardware devices for non-root users: 
 sudo setcap cap_net_raw,cap_net_admin+eip /usr/bin/tcpdump	
+
+sudo systemctl start zabbix-agent && sudo systemctl status zabbix-agent 
+sudo systemctl enable zabbix-agent 
+sudo systemctl enable qemu-guest-agent
+
+git config --global user.name arthurcadore 
+git config --global user.email arthurbarcella.ifsc@gmail.com
+
+# Salvando as credenciais no chaveiro do S.O. Linux (Ubuntu 22.04)
+git config --global credential.helper /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret
+
