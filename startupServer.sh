53164424af3f6dba7d699c266bfa79398fccbc91
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2023-11-16 11:18:15 -0300

    Update startupServer.sh

--- startupServer.sh
+++ startupServer.sh
@@ -4,7 +4,7 @@
 # Update and Upgrade package list: 
 sudo apt-get update && sudo apt-get upgrade -y  
 
-apt-get install -y fping tree arping net-tools vlan zabbix-agent htop unzip qemu-guest-agent
+apt-get install -y fping tree arping net-tools vlan zabbix-agent htop unzip qemu-guest-agent netstat
 
 # Enable capture on hardware devices for non-root users: 
 sudo setcap cap_net_raw,cap_net_admin+eip /usr/bin/tcpdump	
